b8ad8fbd9ac78b19f2b4637b077f268fdf48e66a
anirudhp26 <anirudh26p@gmail.com>
2025-03-23 01:11:22 +0530

    FIX: deployment changes

--- dist/middleware/logger.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = logger;
-function logger(req, res, next) {
-    console.log(`Request URL: ${req.originalUrl} - Method: ${req.method} - Time: ${new Date()}`);
-    next();
-}
-;
-//# sourceMappingURL=logger.middleware.js.map
